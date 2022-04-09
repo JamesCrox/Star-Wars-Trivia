69aff2fbd1a69fef43c7022436747695b6a2b7fe
James Croxson <jaycrox@hotmail.com>
2022-04-09 09:42:08 +0000

    input questions for quiz app

---
+++ questions.py
@@ -0,0 +1,82 @@
+quiz = {
+    1 : {
+        "question" : "How many engines does the X-Wing Fighter have?",
+        "answer" : "Four"
+    },
+    2 : {
+        "question" : "C-3P0 is fluent in how many languages?",
+        "answer" : "60 million"
+    },
+    3 : {
+        "question" : "Who killed the four Jedi Masters: Saesee Tinn, Mace Windu, Kit Fisto, and Agen Kolar?",
+        "answer" : "Darth Sidious"
+    },
+    4 : {
+        "question" : "What is the name of Yoda’s home?",
+        "answer" : "Dagobah"
+    },
+    5 : {
+        "question" : "Who built C-3P0?",
+        "answer" : "Anakin Skywalker"
+    },
+    6 : {
+        "question" : "What is the name of Boba Fett’s ship?",
+        "answer" : "Slave One"
+    },
+    7 : {
+        "question" : "How old is Yoda when he dies?",
+        "answer" : "900"
+    },
+    8 : {
+        "question" : "Who is the young Jedi Knight who becomes Darth Vader?",
+        "answer" : "Anakin Skywalker"
+    },
+    9 : {
+        "question" : "Who killed Han Solo?",
+        "answer" : "Kylo Ren"
+    },
+    10 : {
+        "question" : "Who is the First Order’s supreme leader?",
+        "answer" : "Snoke"
+    },
+    11 : {
+        "question" : "Who was the Jedi master that ordered to create clones?",
+        "answer" : "Syfo Dyas"
+    },
+    12 : {
+        "question" : "What is the name of Han’s friend running the cloud city of Bespin?",
+        "answer" : "Lando Calrissian"
+    },
+    13 : {
+        "question" : "What is the name of Han Solo’s ship?",
+        "answer" : "Millenium Falcon"
+    },
+    14 : {
+        "question" : "Which arm did Count Dooku cut off of Anakin?",
+        "answer" : "Right Arm"
+    },
+    15 : {
+        "question" : "While the Jedi use a number of crystal colors for their lightsabers, what color is preferred by the Sith?",
+        "answer" : "Red"
+    },
+    16 : {
+        "question" : "What is Chewbacca’s weapon of choice?",
+        "answer" : "Bowcaster"
+    },
+    17 : {
+        "question" : "What are the creatures, living on Endor, that helped the Rebel’s to defeat the second Death Star?",
+        "answer" : "Ewoks"
+    },
+    18 : {
+        "question" : "What race does Jar Jar Binks belong to?",
+        "answer" : "Gungan"
+    },
+    19 : {
+        "question" : "What color is the first lightsaber Rey uses?",
+        "answer" : "Blue"
+    },
+    20 : {
+        "question" : "What color was Obi Wan Kenobi’s lightsaber when he defeated Darth Maul?",
+        "answer" : "Green"
+    }
+}
