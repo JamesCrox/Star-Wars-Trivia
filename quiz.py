a916822127444a1e62b3561465a4052a532f7c1c
James Croxson <jaycrox@hotmail.com>
2022-04-09 10:45:08 +0000

    quiz app for final

--- quiz.py
+++ quiz.py
@@ -14,8 +14,10 @@ def check_ans(question, ans, attempts, score):
         print(f"Wrong Answer :( \nYou have {attempts - 1} left! \nTry again...")
         return False
 
-
 def print_dictionary():
+    """
+    Prints out all questions and answers at the end of the app.
+    """
     for question_id, ques_answer in quiz.items():
         for key in ques_answer:
             print(key + ':', ques_answer[key])
@@ -26,14 +28,14 @@ def intro_message():
     Introduces user to the quiz and rules, and takes an input from customer to start the quiz.
     Returns true regardless of any key pressed.
     """
-    name = input("what is your name?\n")
-    print("Welcome to this fun food quiz! \nAre you ready to test your knowledge about food?")
+    print(pyfiglet.figlet_format("Welcome To", justify="center"))
+    print(pyfiglet.figlet_format("Star Wars Trivia\n", justify="center"))
+    name = input("What is your name?\n")
+    print(f"Hi {name}, Welcome to the star wars trivia game!")    
     print("There are a total of 20 questions, you can skip a question anytime by typing 'skip'")
-    input("Press any key to start the fun ;) ")
+    input("Press any key to start the fun ;)\n")
     return True
 
-
-# python project.py
 intro = intro_message()
 while True:
     score = 0
@@ -52,7 +54,8 @@ while True:
 
     break
 
+input("Press enter to see your score ;)\n")
 print(f"Your final score is {score}!\n\n")
-print("Want to know the correct answers? Please see them below! ;)\n")
+input("Want to know the correct answers? Please see them below! ;)\n")
 print_dictionary()
-print("Thanks for playing! 💜")
+print(pyfiglet.figlet_format("Thanks for playing!"))
